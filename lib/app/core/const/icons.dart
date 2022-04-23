b0b3737184371dde7afe239fd01ede261d117efd
unknown <sztukiewicz.lukasz@gmail.com>
2022-04-23 10:50:18 +0200

    theme and widget structure setup

---
+++ lib/app/core/const/icons.dart
@@ -0,0 +1,5 @@
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const FaIcon mapMaker = FaIcon(FontAwesomeIcons.mapMarkerAlt);
+const FaIcon iconSearch = FaIcon(FontAwesomeIcons.search);
+const FaIcon iconLightBulb = FaIcon(FontAwesomeIcons.lightbulb);
---
+++ lib/app/core/const/string.dart
@@ -0,0 +1,24 @@
+const String sAppTitle = "Landify";
+const String sHintTextSearchBar = "Type address here...";
+const String sLabelTextSearchBar = "Street";
+const String sSearch = "Search!";
+const String sAdvancedSearch = "Advanced Search";
+const String sBestPlaceFor = "Best place for: ";
+//const String sSnackBarTitle = "Request sent for addres: ";
+const String sRequestSent = "Request sent for addres: ";
+
+const String sStudents = "students?";
+const String sCuples = "couples?";
+const String sFamilies = "families?";
+const String sCompanyflat = "company flat?";
+const String sSingles = "singles?";
+
+const String sChangeTheme = "Change theme";
+
+const List<String> inhabitants = [
+  sSingles,
+  sStudents,
+  sCuples,
+  sFamilies,
+  sCompanyflat,
+];
